f77455fa7ac4afda78e50bb437e9649b383e8836
GaroBozadjian <garobozadjian@hotmail.com>
2017-10-06 11:03:57 +0400

    practice

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main(){
-  printf("hello,world\n");
+  pid_t child;
+  printf("the main program process ID is %d\n",getpid());
+  child=fork();
+  if(child !=0){
+     printf("Hello World\n");
+     printf("This is the parent process, with an ID %d\n",getpid());
+     printf("The child's process ID id %d\n",child);
+
+  }
+  else
+    printf("This is child process with ID %d\n",getpid());
   return 0;
 }
